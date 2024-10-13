12d0a8bd98af291c562a296ec6298a5facd46a4b
mazharidesky <muhammad.210170230@mhs.unimal.ac.id>
2024-10-13 22:41:05 +0700

    feat: create page and add validasi user

--- app.py
+++ app.py
@@ -1,53 +1,80 @@
-from flask import Flask, render_template, request, redirect
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, request, redirect, session, url_for # type: ignore
+from flask_mysqldb import MySQL # type: ignore
+from functools import wraps
 
 app = Flask(__name__)
 
-# #konfigurasi mysql
-# mysql = MySQL()
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = ''
-# app.config['MYSQL_DB'] = 'uts_pemrograman'
-# app.config['MYSQL_HOST'] = 'localhost'
+# Konfigurasi MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'uts_pemrograman'
+app.config['MYSQL_PORT'] = 3306
 
-# #port
-# app.config['MYSQL_PORT'] = 3306
-
-
-app.config['MYSQL_HOST'] = 'localhost'       # Alamat host (TCP/IP)
-app.config['MYSQL_USER'] = 'root'            # Username MySQL
-app.config['MYSQL_PASSWORD'] = ''  # Password MySQL
-app.config['MYSQL_DB'] = 'uts_pemrograman'     # Nama database
-app.config['MYSQL_PORT'] = 3306              # Port MySQL (default 3306)
+# Konfigurasi secret key untuk session
+app.secret_key = 'kunci_rahasia_anda'  # Ganti dengan kunci rahasia yang aman
 
 mysql = MySQL(app)
 
+# Dekorator untuk memeriksa apakah pengguna sudah login
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'logged_in' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
-#rooting
-@app.route('/', methods=['GET','POST']) 
-def signIn():
-    if request.method == 'POST' : #simpan data
-       _username = request.values.get('username')
-       _email = request.values.get('email')
-       _password = request.values.get('password')
-       
-       sql = "insert into users(name,email , password) values(%s, %s, %s)"
-       data = (_username, _email, _password)
-       
-       cur = mysql.connection.cursor()
-       cur.execute(sql, data)
-       cur = mysql.connection.commit()
-       
-       
-       return redirect('/home.html')
-   
-    else:
-        return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        _username = request.form['username']
+        _password = request.form['password']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE name = %s AND password = %s", (_username, _password))
+        user = cur.fetchone()
+        cur.close()
+        
+        if user:
+            session['logged_in'] = True
+            session['username'] = _username
+            return redirect(url_for('home'))
+        else:
+            return redirect(url_for('login'))
     
-@app.route('/home.html')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        _username = request.form['username']
+        _email = request.form['email']
+        _password = request.form['password']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", (_username, _email, _password))
+        mysql.connection.commit()
+        cur.close()
+        
+        return redirect(url_for('login'))
+    
+    return render_template('register.html')
+
+@app.route('/home')
+@login_required
 def home():
     return render_template('home.html')
-    
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
